263ba1c51508646f3e6e5b9f2e51e7287a4bd6a9
MadhuraPatil22 <madhurap2211@gmail.com>
2026-04-09 15:09:45 +0530

    Add files via upload

---
+++ Implicit_Intent/main/java/com/example/intents/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.intents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+                });
+
+
+            Button btnGoogle=findViewById(R.id.btnGoogle);
+            Button btnWikipedia=findViewById(R.id.btnWikipedia);
+            Button btnChatgpt=findViewById(R.id.btnChatgpt);
+
+            btnGoogle.setOnClickListener(v-> {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com"));
+                startActivity(intent);
+            });
+
+        btnWikipedia.setOnClickListener(v-> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.wikipedia.org"));
+            startActivity(intent);
+        });
+
+        btnChatgpt.setOnClickListener(v-> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://chatgpt.com"));
+            startActivity(intent);
+        });
+    }
+}
